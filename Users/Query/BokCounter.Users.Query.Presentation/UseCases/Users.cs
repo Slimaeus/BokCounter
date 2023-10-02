a120a4b71930b883b23a19aaba68be203e14a3e6
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-10-02 15:38:26 +0700

    Update the UserEndpoints in Query side

--- Users/Query/BokCounter.Users.Query.Presentation/UseCases/Users.cs
+++ Users/Query/BokCounter.Users.Query.Presentation/UseCases/Users.cs
@@ -4,13 +4,16 @@ using MediatR;
 
 namespace BokCounter.Users.Query.Presentation.UseCases;
 
-public class UserEndpoints : ICarterModule
+public class Users : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         var group = app.MapGroup("api/v1/Users")
             .WithTags("Users");
 
-        group.MapGet("", async (ISender sender) => TypedResults.Ok(await sender.Send(new GetAppUsers.Query())));
+        group.MapGet("", async (
+                ISender sender)
+            => TypedResults.Ok(
+                await sender.Send(new GetAppUsers.Query())));
     }
 }
